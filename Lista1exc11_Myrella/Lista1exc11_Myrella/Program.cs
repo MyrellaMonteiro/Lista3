d5c98c68642981a88e70dd1de5b529e2469d9d42
MyrellaMonteiro <Myh.monteiro123@gmail.com>
2026-05-28 18:16:24 -0300

    Add files via upload

---
+++ Lista1exc11_Myrella/Lista1exc11_Myrella/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1exc11_Myrella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int e;
+            Console.WriteLine("Informe o valor do número da base: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o valor do número do exponente: ");
+            e = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} elevado a {1} é igual a: {2}", b, e, Math.Pow(b, e));
+            Console.ReadLine();
+        }
+    }
+}
